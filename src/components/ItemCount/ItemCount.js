81c80f101b1d47891839fa3f1db1ae27b45c23f2
stefhanesl <stefhanesalguero@gmail.com>
2022-09-05 19:27:53 -0700

    Cambios en el boton terminar compra

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 const ItemCount = ({stock, agregarAlCarrito}) => {
 
     const [cantidad, setCantidad] = useState(0);
+    const [loadingBtn, setLoadingBtn] = useState(false)
 
     const onAdd = () => {
         if(cantidad<stock){
@@ -16,7 +17,10 @@ const ItemCount = ({stock, agregarAlCarrito}) => {
             setCantidad(cantidad-1)
         }    
     }
-
+    
+    const mostrarBtn = () => {
+        setLoadingBtn(true)
+    }
 
     return (
         <div className='agregar-guitars'>
@@ -27,18 +31,20 @@ const ItemCount = ({stock, agregarAlCarrito}) => {
             </div>
             <div className='tarjeta-inferior'>
                         <button id='agregar'
-                        onClick={() => agregarAlCarrito(cantidad)}
-                        disabled={cantidad < 1 ? true : false}
+                        onClick={() => mostrarBtn()}
+                        // disabled={cantidad < 1 ? true : false}
                         >Agregar al carrito</button>
 
             </div> 
-            <div className="tarjeta-inferior" to='/cart'>
-                <Link to='/cart' style={{ textDecoration: 'none' }}>
-                    <button id='terminar-compra'
-                                onClick={() => agregarAlCarrito(cantidad)}
-                                >Terminar Compra</button>
-                </Link>
-            </div>
+            {
+                loadingBtn 
+                ?  <div className="tarjeta-inferior" to='/cart'>
+                        <Link to='/cart' style={{ textDecoration: 'none' }}>
+                            <button id='terminar-compra'>Terminar Compra</button>
+                        </Link>
+                    </div>
+                : null
+            }
         
         </div>
     );
